506125b685cb9853706b0b67f533f553f0ebf252
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-15 15:16:43 -0300

    Basic Auth added

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -4,7 +4,7 @@ defmodule RocketpayWeb.AccountsController do
   alias Rocketpay.Account
   alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
 
-  action_fallback RocketpayWeb.FallbackController
+  action_fallback(RocketpayWeb.FallbackController)
 
   def deposit(conn, params) do
     with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
@@ -23,6 +23,8 @@ defmodule RocketpayWeb.AccountsController do
   end
 
   def transaction(conn, params) do
+    # task = Task.async(fn -> Rocketpay.transaction(params) end)
+
     with {:ok, %TransactionResponse{} = transaction} <- Rocketpay.transaction(params) do
       conn
       |> put_status(:ok)
